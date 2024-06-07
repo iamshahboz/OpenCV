b87151bba038efbf939798f5e6a2ff35a9c9046b
Shahboz Abdulkhairov <shahboz.abdulkhairov@mra.com>
2024-06-07 02:40:00 -0700

    image converting and edge cascade

--- basic_functions.py
+++ basic_functions.py
@@ -5,8 +5,12 @@ img = cv.imread('Pictures/resized_image.jpg')
 
 '''Converting the image to grayscale '''
 
-gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-cv.imshow('Gray', gray)
+#gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+#cv.imshow('Gray', gray)
+
+'''Edge Cascade'''
+canny = cv.Canny(img, 125, 175)
+cv.imshow('Canny', canny)
 
 cv.waitKey(0)
 
